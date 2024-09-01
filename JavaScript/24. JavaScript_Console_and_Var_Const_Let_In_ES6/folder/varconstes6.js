f8e37f3dc0dcf74733babd6286635589802114ca
ImNotDanish05 <danishmahdi2005@gmail.com>
2024-09-01 12:32:58 +0700

    xddd

---
+++ JavaScript/24. JavaScript_Console_and_Var_Const_Let_In_ES6/folder/varconstes6.js
@@ -0,0 +1,29 @@
+/* 
+Var
+Const
+ES6
+
+*/
+
+Test1 = 43434343 // not recomended
+console.log(Test1);
+var Test1 = 434343;
+console.log(Test1);
+
+var Test2 = "feef";
+console.log(Test2)
+
+const Const1 = 34343;
+console.log(Const1);
+
+var Test3;
+console.log(Test3)
+Test3 = 34343;
+console.log(Test3)
+
+var Lmaooo = [23,3434,2323, "XDD"];
+console.log(Lmaooo[3])
+console.log(Lmaooo[0])
+Lmaooo[0] = "XDDDDDDDD"
+console.log(Lmaooo[0])
+
