7481c22859b7473556369583d4039fbc4cb4bb63
ImNotDanish05 <danishmahdi2005@gmail.com>
2024-09-04 19:08:30 +0700

    Pls it work

---
+++ JavaScript/26. Datatypes in ES6 Javascript and also Converting Datatypes/Converting Datatypes in ES6.js
@@ -0,0 +1,31 @@
+let obj;
+// var obj;
+// for some reason 'var' still work, heeeh >_<
+
+
+// to compile
+obj = String(8343);
+
+console.log(obj)
+console.log(typeof obj)
+console.log(obj.length)
+
+
+obj = obj.toString(3455)
+
+console.log(obj)
+console.log(typeof obj)
+console.log(obj.length)
+
+obj = Boolean(3434);
+
+console.log(obj)
+console.log(typeof obj)
+console.log(obj.length)
+
+obj = Number("efnfuhehf")
+
+console.log(obj)
+console.log(typeof obj)
+console.log(obj.length)
+
