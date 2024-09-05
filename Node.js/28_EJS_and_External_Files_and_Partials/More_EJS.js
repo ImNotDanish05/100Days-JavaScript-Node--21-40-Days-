b88b85ecbe33d390aa6cc551aa28116cfd7ab525
ImNotDanish05 <danishmahdi2005@gmail.com>
2024-09-05 11:57:36 +0700

    CONFUSING

---
+++ Node.js/28_EJS_and_External_Files_and_Partials/More_EJS.js
@@ -0,0 +1,31 @@
+const express = require("express");
+const software = express();
+
+software.use(express.static("public"))
+software.set("view-engine", "ejs")
+
+software.get("/youtube/:channel/", function(req,res){
+    const channel = req.params.channel;
+    const videoID = [
+        {ID:"YExvqxUQYgE", Title: "Brave"},
+        {ID:"HQrEicGeyuE", Title: "Take it"},
+        {ID:"PU7oaPFZWl4", Title: "Honor"}
+    ]
+    res.render("youtube.ejs", 
+        {
+        channel : channel,
+        videoID : videoID
+        }
+)})
+
+software.get("/:arg1", function(req,res){
+    const arg1 = req.params.arg1;
+    res.render("web", {obj : arg1});
+})
+
+software.get("*/", function(req,res){
+    res.render("home");
+})
+software.listen(2000, function(){
+    console.log("The server is running Lma")
+})
